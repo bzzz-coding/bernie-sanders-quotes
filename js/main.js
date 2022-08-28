7b2159f525398df61c2e6f8921f46df2357a8448
bzzz-coding <86077274+bzzz-coding@users.noreply.github.com>
2022-08-28 09:19:58 -0700

    Added image and css

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
 document.querySelector('#getQuote').addEventListener('click', apiReq)
 
 async function apiReq() {
-
+    
     try {
         const res = await fetch('https://bernie-sanders-quotes-api.herokuapp.com/api')
         const data = await res.json()
-
         console.log(data);
         document.querySelector("#quoteText").textContent = data;
-    } catch(error) {
+    } catch (error) {
         console.log(error);
     }
-    
+
 }
+
+
+
